92584a68bd091c5e3b17c2de251be50b5a101e7a
Isabella Feng <dingding7331@hotmail.com>
2024-04-29 19:19:10 +1200

    basic setups

---
+++ src/Views/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+
+return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Inventory management</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        hot: true,
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+
+    // Resolve configurations
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.tsx', '.wasm']
+    },
+
+};
